e9378cc97e60626a6c77655c2d08a24acb839b8f
Sergey Li <pincats@gmail.com>
2021-03-20 16:38:58 +0300

    feat: Add progress bar with initial styling

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -6,11 +6,18 @@ import { Component, OnInit, Input, Inject } from '@angular/core';
   styleUrls: ['./progress-bar.component.css'],
 })
 export class ProgressBarComponent implements OnInit {
-  @Input() count!: number;
+  @Input() currentQuestionId!: number;
+  questionsRange: number[];
 
-  constructor(
-    @Inject('NUMBER_OF_QUESTIONS') public numberOfQuestions: number
-  ) {}
+  constructor(@Inject('NUMBER_OF_QUESTIONS') public numberOfQuestions: number) {
+    this.questionsRange = Array(this.numberOfQuestions)
+      .fill(0)
+      .map((item, i) => i + 1);
+  }
+
+  isActive(id: number): boolean {
+    return true;
+  }
 
   ngOnInit(): void {}
 }
