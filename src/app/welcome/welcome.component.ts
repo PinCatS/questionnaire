0d1f977e426b387c180fc06a6b9d6169afb20f29
Sergey Li <pincats@gmail.com>
2021-03-21 00:48:47 +0300

    feat: Add inputs for user info and collect it

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+/**
+ * TODO:
+ * 1. Save user information and start in the store
+ * 2. Make fields required
+ * 3. Check input errors
+ * 4. Disable submit button and show errors
+ * 5. Show name in outro and in Results
+ * 6. Redirect to welcome in case !isStarted and user navigated via link
+ * 7. When user reached results, set isStarted to false
+ * 8. Make progress bar clickable
+ * 9. Let users navigate and update the answer
+ * 10. Add more 2 questions
+ * 11. Add list of questions to the store
+ * 12. Split questionnaire into smaller components
+ */
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent implements OnInit {
-  constructor(private router: Router) {}
+  isStarted: boolean;
+  userName!: string;
+  mail!: string;
 
-  startQuest() {
+  constructor(private router: Router) {
+    this.isStarted = false;
+  }
+
+  onSubmit(value: any) {
+    this.isStarted = true;
+    this.userName = value['user-name'];
+    this.mail = value['mail'];
+    console.log(this.isStarted, this.userName, this.mail);
     this.router.navigateByUrl('/questions');
   }
 
